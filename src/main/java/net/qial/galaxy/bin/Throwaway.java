b0a4420bd983d987ac49f76fbfb6103928d22272
Kyle Watson <kw@headwire.com>
2012-09-24 14:39:09 -0700

    Added SeededObject class

--- src/main/java/net/qial/galaxy/bin/Throwaway.java
+++ src/main/java/net/qial/galaxy/bin/Throwaway.java
@@ -1,5 +1,8 @@
 package net.qial.galaxy.bin;
 
+import net.qial.galaxy.world.SeedUtil;
+import net.qial.galaxy.world.Sys;
+
 public class Throwaway {
 
 	/**
@@ -8,10 +11,33 @@ public class Throwaway {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		testworld();
+		buildworld();
+		//testseeds();
 	}
 	
 	public static void testworld() {
+	}
+	
+	public static void buildworld() {
+		String seed = "world12345";
+		Sys sys = new Sys(seed);
+		System.out.println(sys);
+	}
+	
+	public static void testseeds() {
+
+		int min = 1000000;
+		int max = -1000000;
 		
+		for(int i = 0; i < 1000; i++) {
+			String seed = ""+i;
+			int[] arr = SeedUtil.getSeeds(seed);
+			for(int k = 0; k < arr.length; k++) {
+				if(arr[k] > max) max = (int)arr[k];
+				if(arr[k] < min) min = (int)arr[k];
+			}
+		}
+		System.out.println("min: "+min+" max: "+max);
 	}
 
 }
--- src/main/java/net/qial/galaxy/world/SeedUtil.java
+++ src/main/java/net/qial/galaxy/world/SeedUtil.java
@@ -4,9 +4,44 @@ import java.security.MessageDigest;
 
 public class SeedUtil {
 	private static MessageDigest md;
-	private static byte[] getSeedFor(Object o) {
+	public static byte[] getBytes(Object o) {
+		if(md == null) {
+			initMd();
+		}
 		String msg = o.toString();
 		byte[] bytes = md.digest(msg.getBytes());
 		return bytes;
 	}
+	
+	public static int[] getSeeds(Object o) {
+		byte[] arr = getBytes(o);
+		int[] ints = new int[arr.length];
+		// add 128 to everything so its unsigned
+		for(int i = 0; i < arr.length; i++) {
+			ints[i]=((int)arr[i])+128;
+		}
+		return ints;
+	}
+	
+	private static void initMd() {
+		try {
+			md = MessageDigest.getInstance("SHA-1");
+		} catch (Exception e) {
+			// well we're fucked
+		}
+	}
+	
+	public static String toString(byte[] bytes) {
+		StringBuilder sb = new StringBuilder();
+		sb.append("byte[").append(bytes.length).append("],[");
+		for(int i = 0; i < bytes.length; i++) {
+			if(i != 0) {
+				sb.append(",");
+			}
+			sb.append((int)bytes[i]);
+		}
+		sb.append("]");
+		return sb.toString();
+	}
+	
 }
