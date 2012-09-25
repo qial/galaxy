edacd15a0a6ce50f8ebe071bc51a775c4293d919
Kyle Watson <kw@headwire.com>
2012-09-25 00:26:38 -0700

    fixed some formatting

--- src/main/java/net/qial/galaxy/world/SeededObject.java
+++ src/main/java/net/qial/galaxy/world/SeededObject.java
@@ -4,23 +4,29 @@ public abstract class SeededObject {
 	private String seed;
 	private byte[] bytes;
 	private int idx = 0;
+	
 	public SeededObject(String seed) {
 		this.seed = seed;
 	}
+	
 	public void init() {
 		initSeedBytes();
 		initValues();
 	}
+	
 	protected abstract void initValues();
+	
 	protected void initSeedBytes() {
 		bytes = SeedUtil.getBytes(seed);
 	}
+	
 	protected int getNextValue() {
 		int val = (int)bytes[idx];
 		val += 128;
 		idx++;
 		return val;
 	}
+	
 	protected int getNextValue(int mod) {
 		return getNextValue()%mod;
 	}
